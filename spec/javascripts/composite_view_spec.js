3fb57ee067f72d4ba329792334f1f3f1e47aac94
Nick Quaranto <nick@thoughtbot.com>
2011-08-10 17:52:43 -0400

    move js into lib/assets to be rails 3.1 friendly

---
+++ spec/javascripts/composite_view_spec.js
@@ -0,0 +1,10 @@
+describe("CompositeView", function() {
+  beforeEach(function() {
+    $("#test").remove();
+    $("body").append("<div id='test'></div>");
+  });
+
+  xit("works", function() {
+    expect(false).toBeTruthy();
+  });
+});
