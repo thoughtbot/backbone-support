8208a1cfb80e97446323dd7cdf449bab5d3d9b05
Nick Quaranto <nick@thoughtbot.com>
2011-08-10 16:32:47 -0400

    test swapping

--- spec/javascripts/swapping_router_spec.js
+++ spec/javascripts/swapping_router_spec.js
@@ -1,26 +1,53 @@
 describe("SwappingRouter", function() {
-  var router;
+  var historyStarted = false;
+
+  var redView = Backbone.View.extend({
+    render: function() {
+      $(this.el).text("Red!");
+    }
+  });
+
+  var blueView = Backbone.View.extend({
+    render: function() {
+      $(this.el).text("Blue!");
+    }
+  });
+
+  var routerSubclass = Support.SwappingRouter.extend({
+    routes: {
+      "test": "index",
+      "red": "red",
+      "blue": "blue"
+    },
+
+    index: function() {
+    },
+
+    red: function() {
+      this.swap(new redView());
+    },
+
+    blue: function() {
+      this.swap(new blueView());
+    }
+  });
+  var router = new routerSubclass({});
 
   beforeEach(function() {
     window.location.hash = "#";
+    $("#test").remove();
 
-    routerSubclass = Support.SwappingRouter.extend({
-      routes: {
-        "test": "index"
-      },
-
-      index: function() {
-      }
-    });
-    router = new routerSubclass({});
-    Backbone.history.start();
+    if(!historyStarted) {
+      historyStarted = true;
+      Backbone.history.start();
+    }
+    $("body").append("<div id='test'></div>");
+    router.el = $("#test");
   });
 
   it("should be a backbone router", function() {
     var spy = sinon.spy();
     router.bind("route:index", spy);
-
-    waits(50);
 
     runs(function() {
       window.location.hash = "#test"
@@ -30,6 +57,30 @@ describe("SwappingRouter", function() {
 
     runs(function() {
       expect(spy.called).toBeTruthy();
+    });
+  });
+
+  it("renders and swaps backbone views", function() {
+    runs(function() {
+      window.location.hash = "#red"
+    });
+
+    waits(50);
+
+    runs(function() {
+      expect($("#test").text()).toEqual("Red!");
+    });
+
+    waits(50);
+
+    runs(function() {
+      window.location.hash = "#blue"
+    });
+
+    waits(50);
+
+    runs(function() {
+      expect($("#test").text()).toEqual("Blue!");
     });
   });
 });
